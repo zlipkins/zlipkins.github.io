63e7741ec7659fcebc6967c67a2c8362c46993fe
shillingburg <mshillingburg@gmail.com>
2015-01-19 16:34:29 -0500

    Line Color Updated

--- alt/stars.js
+++ alt/stars.js
@@ -179,7 +179,7 @@ function distance(p1, p2) {
 		
 		// Draw the line
 		ctx.beginPath();
-		ctx.strokeStyle = "rgba(10,153,87,"+ (1.0-dist/minDist) +")";
+		ctx.strokeStyle = "rgba(209,36,160,"+ (1.0-dist/minDist) +")";
 		ctx.moveTo(p1.x, p1.y);
 		ctx.lineTo(p2.x, p2.y);
 		ctx.lineWidth = 1;
